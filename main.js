7d97e23760f6ef38fa0ac092240e280f9cd9bbb6
unknown <subaanfeetpics@gmail.com>
2022-04-17 10:24:24 +1000

    initialise repository

---
+++ main.js
@@ -0,0 +1,16 @@
+﻿const { Client, Intents } = require('discord.js');
+require("dotenv").config()
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}`)
+});
+
+client.on("messageCreate", (message) => {
+    if (message.content == "hello"){
+        message.reply("bro what")
+    }
+})
+
+client.login(process.env.TOKEN)

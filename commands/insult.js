6b311805f0da9feee2e35066e38a6ad476a9b03b
Suhaan <101968109+Crotchwhistle@users.noreply.github.com>
2022-05-06 20:51:19 +1000

    fixed insult command

--- commands/insult.js
+++ commands/insult.js
@@ -1,17 +1,11 @@
 const { MessageEmbed } = require('discord.js')
 
 module.exports = {
-    calback: (message, text) => {
+    callback: (message, text) => {
         insult(message);
 
         function insult(message) {
-            var reponses = [
-                "Stfu gay faggot",
-                "Stfu wanna fight",
-                "Have you had ali baba before?",
-                "Oml",
-                "Stfu you fat bitch"
-            ]
+            var responses = ["Stfu gay faggot", "Stfu wanna fight", "Have you had ali baba before?", "Oml", "Stfu you fat bitch"]
             var rndmiser = Math.floor(Math.random() * responses.length)
 
             message.reply(responses[rndmiser]);
